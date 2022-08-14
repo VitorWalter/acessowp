c0d18545c1ecaf4373649482de204c3219397c44
VitorWalter <vitor_lisboa25@hotmail.com>
2022-08-14 11:34:25 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# acessowp" 
---
+++ acessowp.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#
+# acessowp.sh  
+#
+# Autor:Vitor Walter
+#
+# ------------------------------------------------------------------------ #
+#  Este programa irá gerar o acesso ao administrador do Wordpress
+#  
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- VARIÁVEIS ----------------------------------------- #
+dominio=$1
+acesso=$(cat wp-config.php | grep "DB_NAME" | awk -F " " {'print$3'} | tr -d "'")
+raiz=$(cwd $dominio && pwd)
+server_domain=$(cat /etc/userdomains | grep "$dominio" | awk -F":" {'print $1'})
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- TESTES ----------------------------------------- #
+if[ $# -ne 1 ];
+    then
+        echo "sintase incorreta. 
+        Digite a seguinte sintase acessowp.sh <dominio>" 
+        exit 1
+        
+fi
+
+if [ "$dominio" = ]
+
+if [ "$dominio" = "$server_domain" ]
+then
+    echo "domínio existe no servidor"
+else
+    echo "domínio não existe no servidor"
+fi
+
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- EXECUÇÃO ----------------------------------------- #
+cwd $dominio
+pwtemp setwp $acesso
+# ------------------------------------------------------------------------ #
